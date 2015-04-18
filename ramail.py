d7caf49add08f9bda978a0f5ce38cde0a5f2f6eb
MrRyanAlexander <lazyguymedia@gmail.com>
2015-04-18 11:03:22 -0700

    Initial Commit

---
+++ ramail.py
@@ -0,0 +1,27 @@
+'''
+  Created By    : Ryan Alexander
+  Date          : 4/17/2015
+  FileName      : ramail.py
+  Description   : A program to send SMTP message via Mailgun API
+'''
+import requests
+
+from keys import kApiKey
+
+def send_simple_message(address, subject, body):
+    return requests.post(
+        "https://api.mailgun.net/v3/ryan-alex.com/messages",
+        auth=("api", kApiKey),
+        data={"from": "Ryan <ryan@ryan-alex.com>",
+            "to": address,
+            "subject": subject,
+            "text": body })
+
+def main(): 
+  address = raw_input("Email: ")
+  subject = raw_input("Subject: ")
+  body    = raw_input("Message: ")  
+  send_simple_message(address, subject, body)
+
+if __name__ == "__main__":
+    main()
