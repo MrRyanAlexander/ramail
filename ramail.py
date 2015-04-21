2f4e17498ee32879249fb25ceca1b3d7af16f6ca
MrRyanAlexander <lazyguymedia@gmail.com>
2015-04-21 14:37:45 -0700

    Updated ramail.py

--- ramail.py
+++ ramail.py
@@ -12,15 +12,17 @@ def send_simple_message(address, subject, body):
     return requests.post(
         "https://api.mailgun.net/v3/ryan-alex.com/messages",
         auth=("api", kApiKey),
+        files=[("attachment", "RyanResumeWord.docx")],
         data={"from": "Ryan <ryan@ryan-alex.com>",
             "to": address,
             "subject": subject,
             "text": body })
 
 def main(): 
-  address = raw_input("Email: ")
-  subject = raw_input("Subject: ")
-  body    = raw_input("Message: ")  
+  address  = raw_input("Email: ")
+  subject  = raw_input("Subject: ")
+  body     = raw_input("Message: ") 
+
   send_simple_message(address, subject, body)
 
 if __name__ == "__main__":
