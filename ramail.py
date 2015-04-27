5d211e70c78ed49bb9afe9bd3ff93ee1fe99b8c2
MrRyanAlexander <lazyguymedia@gmail.com>
2015-04-26 21:33:57 -0700

    Updated to include new Resume

--- ramail.py
+++ ramail.py
@@ -12,11 +12,11 @@ def send_simple_message(address, subject, body):
     return requests.post(
         "https://api.mailgun.net/v3/ryan-alex.com/messages",
         auth=("api", kApiKey),
-        files=[("attachment", "RyanResumeWord.docx")],
+        files=[("attachment", open("RyanResume.docx"))],
         data={"from": "Ryan <ryan@ryan-alex.com>",
             "to": address,
             "subject": subject,
-            "text": body })
+            "html": body })
 
 def main(): 
   address  = raw_input("Email: ")
