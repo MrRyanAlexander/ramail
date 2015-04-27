491c0216d4fef2c0b96c3345afee1e3c0c6c8c30
MrRyanAlexander <lazyguymedia@gmail.com>
2015-04-27 00:56:02 -0700

    Organized the folder structure, updated ramail.py

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-<strong>ramail.py</strong>
+#ramail.py
 
-- Uses Mailgun <a href="https://documentation.mailgun.com/quickstart-sending.html#send-via-api">API</a>
+Send the contents of an html file as a request to the Mailgun API. 
+A recipient email address using the -a flag and a subject using the
+-s flag must be set. 
 
-- Run as python ramail.py
+'''bash
+python ramail.py -a "example@email.com" -s "Subject"
+'''
+
+I need a way to create an html/txt file for each email I want to send and I want to store it in a folder locally. The kind of thing I want to have is similar the following structure: 
+
+_dir: 
+
+	_drafts:
+		draft-someone@-site-com.html
+
+	_ready:
+		2015-04-26-someone@-site-com.txt
+
+	_sent:
+		2015-04-26-someone@-site-com.html
 	
-- Program asks for 
-	- Email 
-	- Subject 
-	- Message
+	_received:
+		2015-04-26-someone@-site-com.html
 
-- @Terminates with string success || error
+Basically what would happen above is markdown would be converted into a string and stored in a single line text file. That file would then used to send the actual message. Once the file is sent it would be converted back into either markdown or an html file. Incoming message are in the form of text and would need to be converted into either markdown or html as well. This would also make it so that the html files can be used to interfact with this graphically, but I wouldn't want that personally. Finally there would need to be a way to check for new messages and I would love to have something in the terminal that alerts me when new messages arrive and even let me view them, reply to them, delete them or just save them for later; all from the command line. 
--- ramail.py
+++ ramail.py
@@ -2,28 +2,39 @@
   Created By    : Ryan Alexander
   Date          : 4/17/2015
   FileName      : ramail.py
-  Description   : A program to send SMTP message via Mailgun API
+  Description   : A program to send message via Mailgun API
 '''
 import requests
+from argparse import ArgumentParser
+from keys.keys import kApiKey
 
-from keys import kApiKey
-
-def send_simple_message(address, subject, body):
+def send_simple_message(address, subject):
     return requests.post(
         "https://api.mailgun.net/v3/ryan-alex.com/messages",
         auth=("api", kApiKey),
-        files=[("attachment", open("RyanResume.docx"))],
+        files=[("attachment", open("_attachments/RyanResume.docx")),
+        ("attachment", open("_attachments/RyanResume.pdf"))],
         data={"from": "Ryan <ryan@ryan-alex.com>",
             "to": address,
             "subject": subject,
-            "html": body })
+            "html": open("_ready/stage.html") })
 
 def main(): 
-  address  = raw_input("Email: ")
-  subject  = raw_input("Subject: ")
-  body     = raw_input("Message: ") 
-
-  send_simple_message(address, subject, body)
+  parser = ArgumentParser(description="""\
+Send the contents of an html file as a request to the Mailgun API. 
+A recipient email address using the -a flag and a subject using the
+-s flag must be set. 
+""")
+  parser.add_argument('-a', '--address', required=True,
+                        help="""Set a recipient email address to send
+                        this message to.""")
+  parser.add_argument('-s', '--subject', required=True,
+                        help="""Set a subject to send with this message.""")
+  
+  args = parser.parse_args()
+  address = args.address
+  subject = args.subject
+  send_simple_message(address, subject)
 
 if __name__ == "__main__":
     main()
